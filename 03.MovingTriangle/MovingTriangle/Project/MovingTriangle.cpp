2c8dc37f75f9f65419e6e24be8cd2af144872a3c
emilystrange93 <emilystrange93@gmail.com>
2022-04-28 20:48:20 +0200

    Moving Triangle finalized

--- 03.MovingTriangle/MovingTriangle/Project/MovingTriangle.cpp
+++ 03.MovingTriangle/MovingTriangle/Project/MovingTriangle.cpp
@@ -36,9 +36,9 @@ int main() {
 	Shader ourShader("shader.vert", "shader.frag");
 
 	float vertices[] = {
-		 .5f, -.5f, .0f,
-		-.5f, -.5f, .0f,
-		 .0f,  .5f, .0f
+		 .05f, -.05f, .0f, 1.f, 0.101f, 0.666f,
+		-.05f, -.05f, .0f, 1.f, 0.101f, 0.666f,
+		 .0f,  .05f, .0f, .0f, .0f, .0f
 	};
 
 	unsigned int VBO, VAO;
@@ -50,8 +50,11 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -60,18 +63,24 @@ int main() {
 		glClearColor(.176f, .035f, .227f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		double timeValue = glfwGetTime();
-		float redValue = static_cast<float>(sin(timeValue) / 2. + .5);
-		ourShader.setFloat("xOffset", redValue);
-		ourShader.setFloat("ourColor", redValue);
-		
+		float timeValue = glfwGetTime()/2;
+		float xOffset = sin(timeValue)/1.2;
+		float yOffset = cos(timeValue*3.14) / 1.2;
+		float colOffset = (sin(timeValue)/2 + 0.5f);
 		ourShader.use();
+		ourShader.setFloat("colOffset", colOffset);
+		ourShader.setFloat("xOffset", xOffset);
+		ourShader.setFloat("yOffset", yOffset);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1,&VBO);
 
 	glfwTerminate();
 	return 0;
--- 03.MovingTriangle/MovingTriangle/Project/shader.frag
+++ 03.MovingTriangle/MovingTriangle/Project/shader.frag
@@ -1,8 +1,8 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform float ourColor;
+in vec3 ourColor;
 
 void main(){
-	FragColor = vec4(ourColor, .0f, .721f, .8f);
+	FragColor = vec4(ourColor, 1.f);
 }
